25c9e48ded006a712a50232d1f861d08c36ca9cd
v1aD0S34 <144205052+v1aD0S34@users.noreply.github.com>
2024-01-23 16:01:59 +0300

    ЛАЙТ РЕФАКТОРИНГ

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import warnings
+
 from excelLogics import process_excel_file
 from generationTrends import generate
 
 
 if __name__ == "__main__":
+    warnings.filterwarnings('ignore', category=UserWarning, module='openpyxl')
     try:
         generate()
     except Exception as e:
